b5d8b43fcbb18c6ba1c7845c39c9f2e885fd6060
dhiraj <dhirajdhas@25gmail.com>
2023-02-20 00:12:36 +0530

    compent and style

---
+++ practice1/src/component/Header.jsx
@@ -0,0 +1,24 @@
+import Footer from "./Footer";
+
+  const Header = ()=> {
+    let name = 'Header Component';
+  return (
+    <div className="header">
+      <h1>{name}</h1>
+      <span style={{color:"black  "}}>Block of code and reusable the react Component</span>
+      <Footer />
+    </div>
+  )
+}
+export default Header;
+
+// const Footer = () => {
+//   let name = 'Footer Component';
+//   return (
+//     <div>
+//       <h3>{name}</h3>
+//     </div>
+//   )
+// }
+
+// export {Header ,Footer} ;
