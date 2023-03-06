94a060b627d5fce3874bb3094b5bc0dfb563d5b1
Dhiraj Dhas <dhiraj.dhas@coreviewsystems.com>
2023-03-06 11:34:23 +0530

    custructor in react

--- practice5/src/components/Comp2.jsx
+++ practice5/src/components/Comp2.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 export default class Comp2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.inc = this.inc.bind(this);
+  }
+
+  inc() {
+    this.setState({ count: this.state.count + 2 });
+  }
+
   componentWillUnmount() {
     alert("Remove the component");
   }
   render() {
     return (
       <div>
-        <h1>Child Component</h1>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.inc}>Count constructor</button>
       </div>
     );
   }
